1bfc916fadc7dbd742a14adf87e1a71ee84be665
Patrick Marie <pm@mkz.me>
2024-05-06 23:07:29 +0200

    chore: removing nixpkgs-fluxcd

--- flake.nix
+++ flake.nix
@@ -7,14 +7,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # for flux-cd 2.0.1
-    nixpkgs-fluxcd.url = "github:nixos/nixpkgs/976fa3369d722e76f37c77493d99829540d43845";
   };
 
   outputs =
     inputs@{ nixpkgs
-    , nixpkgs-fluxcd
     , home-manager
     , ...
     }:
@@ -36,10 +32,6 @@
           ];
           extraSpecialArgs = {
             enableJobFeatures = true;
-
-            versions = {
-              pkgs-fluxcd = nixpkgs-fluxcd.legacyPackages.${system};
-            };
           };
 
           inherit pkgs;
