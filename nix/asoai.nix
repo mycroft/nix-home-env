ddbf464674cdbfb4e93ceecd7bc0bcffa98f3825
Patrick Marie <pm@mkz.me>
2024-05-21 09:36:29 +0200

    feat(asoai): adding shell completions

--- nix/asoai.nix
+++ nix/asoai.nix
@@ -1,5 +1,6 @@
 { buildGoModule
 , fetchFromGitHub
+, installShellFiles
 ,
 }:
 buildGoModule rec {
@@ -7,12 +8,23 @@ buildGoModule rec {
   version = "0.0.1";
   vendorHash = "sha256-CC6Yf/zNNYoFVqiv/D1TWdbEiOmWRMUuCHO9fG2n2FU=";
 
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
   src = fetchFromGitHub {
     owner = "mycroft";
     repo = "asoai";
     rev = "${version}";
     sha256 = "sha256-bh8blqIXPGQyoWv52CrP1W/xpSv2H7W9HmSYkwtMt0A=";
   };
+
+  postInstall = ''
+    installShellCompletion --cmd asoai \
+      --bash <($out/bin/asoai completion bash) \
+      --fish <($out/bin/asoai completion fish) \
+      --zsh <($out/bin/asoai completion zsh)
+  '';
 
   meta = {
     description = "Another stupid Open AI client";
