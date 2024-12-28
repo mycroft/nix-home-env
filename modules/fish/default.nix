59c64ee8123e5a6fbfd945757f17cdd607840310
Patrick Marie <pm@mkz.me>
2024-12-28 09:38:04 +0100

    fix: remove k8s info in shell prompt

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -33,7 +33,7 @@
 
       set -g theme_display_date no
       set -g theme_display_cmd_duration no
-      set -g theme_display_k8s_context yes
+      set -g theme_display_k8s_context no
       set -g theme_display_k8s_namespace no
     '';
 
