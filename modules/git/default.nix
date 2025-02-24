557632ec3e5cd54efaff5bf815b2e90aee4901f0
Patrick MARIE <pm@mkz.me>
2025-02-24 23:27:10 +0100

    feat: misc git config update

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -12,6 +12,7 @@
       hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
       lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       st = "status";
+      sw = "switch";
       staged = "diff --cached";
       last = "rev-parse HEAD";
     };
