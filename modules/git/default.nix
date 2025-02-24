e4bb6f5a4796c431ca5a948b627b673047b9a660
Patrick MARIE <pm@mkz.me>
2025-02-24 23:03:22 +0100

    feat: enhance git configuration

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -2,11 +2,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "Patrick Marie";
+    userName = "Patrick MARIE";
     userEmail = "pm@mkz.me";
     aliases = {
       br = "branch";
       co = "checkout";
+      cm = "commit";
       gra = "gra = log --pretty=format:'\"%Cgreen%h %Creset%cd %C(bold blue)[%cn] %Creset%s%C(yellow)%d%C(reset)\"' --graph --date=relative --decorate --all";
       hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
       lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
@@ -16,23 +17,58 @@
     };
     extraConfig = {
       branch.autosetuprebase = "always";
-      core.editor = "nvim";
-      github.user = "mycroft";
-      help.autocorrect = 1;
-      init.defaultBranch = "main";
       color = {
         ui = true;
         pager = true;
       };
+      commit = {
+        gpgSign = true;
+        verbose = true;
+      };
+      core = {
+        editor = "nvim";
+        excludesfiles = "~/.gitignore";
+      };
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      github.user = "mycroft";
+      help.autocorrect = 1;
+      init.defaultBranch = "main";
+      merge = {
+        conflictstyle = "zdiff3";
+      };
+      pull.rebase = true;
+      push = {
+        autoSetupRemote = true;
+        default = "simple";
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      ui.auto = true;
       url = {
         "ssh://git@git.mkz.me" = {
           insteadOf = "https://git.mkz.me";
         };
       };
-      pull.rebase = true;
-      push.autoSetupRemote = true;
-      push.default = "tracking";
     };
+    ignores = [
+      "*~"
+      "*.swp"
+      "/target"
+    ];
     includes = [
       {
         condition = "gitdir:~/custocy/";
@@ -42,7 +78,6 @@
           };
           user = {
             email = "pmarie@custocy.com";
-            name = "Patrick MARIE";
             signingKey = "D5A4B4A32C8FC68F92A4A83340224FD197FAAD5B";
           };
           commit = {
