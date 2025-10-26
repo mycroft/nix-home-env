da17315077a31f696a9ad2d32965a687e1a0df00
Patrick MARIE <pm@mkz.me>
2025-10-26 12:02:43 +0100

    refactor: migrate git config to settings format
    Reorganizes git configuration to use the newer `settings` format instead of
    separate fields and `extraConfig`. Moves user info and aliases into the settings
    structure while maintaining the same functionality.

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -2,24 +2,22 @@
 {
   programs.git = {
     enable = true;
-    userName = "Patrick MARIE";
-    userEmail = "pm@mkz.me";
-    aliases = {
-      br = "branch";
-      co = "checkout";
-      cm = "commit";
-      gra = "gra = log --pretty=format:'\"%Cgreen%h %Creset%cd %C(bold blue)[%cn] %Creset%s%C(yellow)%d%C(reset)\"' --graph --date=relative --decorate --all";
-      hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
-      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      st = "status";
-      sw = "switch";
-      staged = "diff --cached";
-      last = "rev-parse HEAD";
-    };
     lfs = {
       enable = true;
     };
-    extraConfig = {
+    settings = {
+      alias = {
+        br = "branch";
+        co = "checkout";
+        cm = "commit";
+        gra = "gra = log --pretty=format:'\"%Cgreen%h %Creset%cd %C(bold blue)[%cn] %Creset%s%C(yellow)%d%C(reset)\"' --graph --date=relative --decorate --all";
+        hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        st = "status";
+        sw = "switch";
+        staged = "diff --cached";
+        last = "rev-parse HEAD";
+      };
       branch.autosetuprebase = "always";
       color = {
         ui = true;
@@ -52,12 +50,12 @@
         pruneTags = true;
         all = true;
       };
-      interactive = {
-        diffFilter = "delta --color-only";
-      };
       github.user = "mycroft";
       help.autocorrect = 1;
       init.defaultBranch = "main";
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
       merge = {
         conflictstyle = "zdiff3";
       };
@@ -78,7 +76,10 @@
           insteadOf = "https://git.mkz.me";
         };
       };
-
+      user = {
+        name = "Patrick MARIE";
+        email = "pm@mkz.me";
+      };
     };
     ignores = [
       "*~"
@@ -88,29 +89,29 @@
       "__pycache__/"
     ];
     includes = builtins.map
-    (dir:
-      {
-        condition = "gitdir:${dir}";
-        contents = {
-          core = {
-            sshCommand = "ssh -i ~/.ssh/work/id_ed25519 -F /dev/null";
-          };
-          user = {
-            email = "pmarie@custocy.com";
-            signingKey = "D5A4B4A32C8FC68F92A4A83340224FD197FAAD5B";
-          };
-          commit = {
-            gpgSign = true;
-          };
-          credential = {
-            "https://git-codecommit.eu-west-3.amazonaws.com" = {
-              helper = "!aws codecommit --profile root credential-helper $@";
-              useHttpPath = true;
+      (dir:
+        {
+          condition = "gitdir:${dir}";
+          contents = {
+            core = {
+              sshCommand = "ssh -i ~/.ssh/work/id_ed25519 -F /dev/null";
+            };
+            user = {
+              email = "pmarie@custocy.com";
+              signingKey = "D5A4B4A32C8FC68F92A4A83340224FD197FAAD5B";
+            };
+            commit = {
+              gpgSign = true;
+            };
+            credential = {
+              "https://git-codecommit.eu-west-3.amazonaws.com" = {
+                helper = "!aws codecommit --profile root credential-helper $@";
+                useHttpPath = true;
+              };
             };
           };
-        };
-      }
-    )  [ "~/work/" "/opt/custocy/" ];
+        }
+      ) [ "~/work/" "/opt/custocy/" ];
 
     signing = {
       key = "A438EE8E0F1C6BAA21EB8EB4BB519E5CD8E7BFA7";
