6b322c3d59959109c69dd04eece2b5a420c605a0
Patrick MARIE <pm@mkz.me>
2026-08-18 20:09:57 +0200

    feat: disabling mouse mode in nvim
    While I am at it, let's refresh flakes.

--- modules/neovim/files/init.lua
+++ modules/neovim/files/init.lua
@@ -32,9 +32,9 @@ vim.opt.smarttab = true
 vim.api.nvim_command('filetype plugin indent on')
 
 -- enables mouse in normal, visual, insert modes
-vim.opt.mouse = "nvi"
+vim.opt.mouse = ""
 -- use both "*" and "+" registers for yank/paste operations
-vim.opt.clipboard = "unnamed,unnamedplus"
+-- vim.opt.clipboard = "unnamed,unnamedplus"
 
 vim.api.nvim_set_keymap('n', '<C-n>', ':tabnext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-p>', ':tabprevious<CR>', { noremap = true, silent = true })
